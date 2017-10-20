1de5616af81dc778cb13775a4a4bee4d2d807b2b
rcarlier <rcarlier@GRALAP024.nalys.local>
2017-10-20 18:44:04 +0200

    Added Test package & first class

---
+++ test/service/GameTest.java
@@ -0,0 +1,34 @@
+package service;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMainDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOptions() {
+		fail("Not yet implemented");
+	}
+
+}
