7908d5a90fb6b46f6fb18910180737f41a201965
User <User@DESKTOP-Q65SJCI.lan>
2017-11-01 23:15:04 +0100

    created new light test for page repository

---
+++ test/repository/PageRepositoryTest.java
@@ -0,0 +1,28 @@
+package repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import repository.PageRepository;
+
+public class PageRepositoryTest {
+	
+	private static PageRepository pageRepository = new PageRepository();
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testReadFromJson() {
+		
+	}
+
+}
